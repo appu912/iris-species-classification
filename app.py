13cd9d8699d1ef1b2ec6a4801425c68bc40ac302
appu912 <mehra.appu912@gmail.com>
2021-08-01 09:46:36 +0530

    updated

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ model = joblib.load(open('iris.joblib' , 'rb'))
 
 app = Flask(__name__)
 
-@app.route('https://iris-species-prd.herokuapp.com/' , methods = ["GET"])
+@app.route('/' , methods = ["GET"])
 def Home():
     return render_template('index.html' , data = "" , c = "")
 
-@app.route('https://iris-species-prd.herokuapp.com/predict' , methods = ['POST'])
+@app.route('/predict' , methods = ['POST'])
 def predict():
     sepalLength = request.form['sepalLength']
     sepalWidth = request.form['sepalWidth']
